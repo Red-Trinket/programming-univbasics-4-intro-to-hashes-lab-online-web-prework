ea1063656d506dbbe1251486556192ea609f8b35
Matthew Jones <matts3371@gmail.com>
2019-09-05 00:56:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,21 @@
 def new_hash
   # return an empty hash
+  empty = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  mine = {name: "My Precious"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pion = {:name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  generator = {:id => 5}
 end
 
 def my_hash_creator(key, value)
